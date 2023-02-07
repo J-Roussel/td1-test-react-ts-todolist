aa38e5822de4d84200bbe6aa454ffcef886fc286
J-Roussel <hei.roussel11@gmail.com>
2023-02-08 01:46:23 +0300

    feat: add test for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,112 @@
-import { useState } from 'react';
-import TodoAddForm from './components/TodoAddForm';
-import TodoItemList from './components/TodoItemList';
-import Todo, { TodoStatus } from './utils/models/Todo';
+import React, {useState} from 'react';
+import './App.css';
+import Form from './components/form/Form';
+import Card from './components/card/card';
+import { ITask } from "./utils/interfaces";
+
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodo = (newTodo: Todo) => {
-    setTodos(currentTodos => [...currentTodos, newTodo]);
-  };
 
-  const removeTodo = (todoId: number) => {
-    setTodos(currentTodos => currentTodos.filter(todo => todo.id !== todoId));
-  };
+  //Default values of the the inputs of the form
+  let initialValue: string[] = ["Name of the task...", "Description of the task..."];
+  const [valueName, setValueName] = useState<string>("");
+  const [valueDescri, setValueDescri] = useState<string>("");
 
-  const doneTodo = (todoId: number) => {
-    const editTodos = (currentTodos: Todo[]) => {
-      return currentTodos.map(todo => (todo.id !== todoId ? todo : { ...todo, status: TodoStatus.DONE }));
-    };
-    setTodos(editTodos);
-  };
+  //useState for the ToDoList
+  const [toDoList, setToDo] = useState<ITask[]>([]);
+
+  const [statut, setStatut] = useState<boolean>(false);
+
+  // mark as done
+  const asDoneBtn = ():void => {
+    setStatut(!statut);
+  }
+
+  //onChange function
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>):void => {
+    (e.target.name==='inputName')? setValueName(e.currentTarget.value) : setValueDescri(e.currentTarget.value);
+  };  
+
+  //function that allows to add the task given by user in the ToDo card
+  const addTask = ():void => {
+    const newTask = {name:valueName, description:valueDescri};
+    setToDo([...toDoList, newTask]);
+    setStatut(false);
+    setValueName("");
+    setValueDescri("");
+  }
+
+  //function that allows to remove the task from its card
+  const removeTask = (taskNameToDelete:string):void => {
+    setToDo(toDoList.filter((task:ITask) => {
+      return (task.name !== taskNameToDelete);
+    }));
+  }
 
   return (
-    <div className='container' style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ display: 'flex', width: '50vw', justifyContent: 'space-around' }}>
-        <TodoItemList
-          actions={<TodoAddForm add={addTodo} />}
-          todoStatus={TodoStatus.TODO}
-          todos={todos}
-          onDone={doneTodo}
-          onRemove={removeTodo}
+    <React.Fragment>
+      <div className='container'>
+        <h1>ToDo List React Typescript</h1>
+        <Form valueName={valueName} 
+              valueDescri={valueDescri} 
+              initialValue={initialValue}
+              handleChange={handleChange}
+              addTask={addTask}
         />
-        <TodoItemList todoStatus={TodoStatus.DONE} todos={todos} onDone={doneTodo} onRemove={removeTodo} />
+
+        <div className='card'>
+          <div className="left-card">
+            <Card titre="ToDo" id="left-card-title">
+              <>
+                {toDoList.map((task:ITask) => {
+                  if((task.name !== "" || task.description !== "") && statut === false){
+                    return (
+                      <div className="task">
+                        <div className="task-data" >
+                          <h4>{task.name}</h4>
+                          <p className="taskDescription">{task.description}</p>
+                        </div>
+                        <div className="action-btn">
+                              <input className="completed-btn" type="button" value="completed" onClick={asDoneBtn}/>
+                              <input className="remove-btn"  type="button" value="remove"onClick={()=>{removeTask(task.name)}}/>
+                        </div>
+                      </div>                
+                    );
+                  }
+                  return "";
+                })}
+              </>
+            </Card>
+          </div>
+
+          <div className=''>
+            <Card titre='Done' id='right-card-title'>
+              <>
+                {toDoList.map((task:ITask) => {
+                      if(statut === true){
+                        return (
+                          <div className="task">
+                            <div className="task-data" >
+                              <h4>{task.name}</h4>
+                              <p className="taskDescription">{task.description}</p>
+                            </div>
+                            <div className="action-btn">
+                                  <input className="remove-btn"  type="button" value="remove"onClick={()=>{removeTask(task.name)}}/>
+                            </div>
+                          </div>                
+                        );
+                      }
+                    })}
+                
+                </>
+              </Card>
+          </div>
+
+        </div>
+
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/tests/utils/selectors.ts
+++
@@ -1,4 +0,0 @@
-export const ID_TODO_DONE = "input-todo-done-id";
-export const ID_TODO_DELETE = "input-todo-delete-id";
-export const ID_TODO_ADD = "input-todo-add-id";
-export const ROLE_TODO_DELETE = "input-todo-delete-role";
